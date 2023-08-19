7b22fb029600b376883218998bee2983e6cf3c01
شَکِیْب | Shakib <52566642+Shakib-BD@users.noreply.github.com>
2023-08-19 11:24:52 +0600

    voltage bringup

--- voltage_merlinx.mk
+++ voltage_merlinx.mk
@@ -11,10 +11,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/xiaomi/merlinx/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Electrifying stuff.
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_merlinx
+# VoltageOS Stuffs
+VOLTAGE_BUILD_TYPE := UNOFFICIAL
+TARGET_BUILD_GRAPHENEOS_CAMERA := true
+TARGET_BOOT_ANIMATION_RES := 1280
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_SUPPORTS_QUICK_TAP := true
+
+PRODUCT_NAME := voltage_merlinx
 PRODUCT_DEVICE := merlinx
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
